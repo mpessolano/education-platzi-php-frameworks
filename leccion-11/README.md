8efe4e52810cef35e8f1b65a6ecd438bec5e8d6c
Marianna Pessolano <marianna.pessolano@gmail.com>
2021-02-22 22:04:58 -0400

    Lección 11: Repaso final

---
+++ leccion-11/README.md
@@ -0,0 +1,5 @@
+## Curso de Introducción a Frameworks de PHP
+
+### Lección 10: Helpers
+
+https://platzi.com/clases/2033-php-frameworks/32621-helpers/
---
+++ leccion-11/views/home.php
@@ -0,0 +1,4 @@
+<div class="col-8">
+    <h1>Página Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ea dolor officia quia, asperiores ducimus dolores provident dolore soluta incidunt officiis eligendi voluptatem similique sit! Totam veritatis ad iste quo.</p>
+</div>
